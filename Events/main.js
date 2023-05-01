5d06175bfbb0407bd9cbce2fdfb53c0bb26786b3
Carlos Jesus Caro <carlosjesuscaro@icloud.com>
2023-05-01 00:43:12 -0400

    Basic DOM: functions based on events

--- Events/main.js
+++ Events/main.js
@@ -15,3 +15,45 @@ function addP(){
 }
 
 var button = document.getElementById("AddP").addEventListener("click", addP);
+
+/*
+This is one way to do it
+
+function mouseEnter(){
+    let target = document.getElementById("mouseEnterLeave");
+    target.style.color = "red";
+}
+
+function mouseLeave(){
+    let target = document.getElementById("mouseEnterLeave");
+    target.style.color = "black";
+}
+
+document.getElementById("mouseEnterLeave").addEventListener("mouseenter", mouseEnter);
+document.getElementById("mouseEnterLeave").addEventListener("mouseleave", mouseLeave);
+
+*/
+
+// Here is another way:
+function mouseEnter(event){
+    event.srcElement.style.color = "red";
+}
+
+function mouseLeave(event){
+    event.srcElement.style.color = "black";
+}
+
+let enterLeave = document.getElementsByClassName("mouseEnterLeave");
+
+for (let div of enterLeave){
+    div.addEventListener("mouseenter", mouseEnter);
+    div.addEventListener("mouseleave", mouseLeave);
+}
+
+function scrollSquare(event){
+    //console.log(event)
+    let square = document.getElementById("squareToMove");
+    square.style.left = square.offsetLeft + event.deltaY + 'px';
+}
+
+document.getElementById("scrollContainer").addEventListener("wheel", scrollSquare);
